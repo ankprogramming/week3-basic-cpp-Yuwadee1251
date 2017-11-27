bdd5d6e4ba72d185abed4bcf14e366bb6f0bb2c9
Unknown <yuwadee.dd58@gmail.com>
2017-11-27 18:10:51 +0700

    แก้ไขข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss.Yuwadee Ponok" << endl;
+  cout <<"Nickname : Ja" << endl;
+  cout <<"Sax : Male" << endl;
+  cout <<"Birth : 18/July/1998" << endl;
+  cout <<"Hobbies : Play games and Read Book " << endl;
+  cout <<"Status : Single " << endl;
   return 0;
 }
