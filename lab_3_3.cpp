3aff240c59dcee10f9549f29e51c9dd5f4eaec68
Unknown <yuwadee.dd58@gmail.com>
2017-11-28 11:36:45 +0700

    แก้ไขข้อ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main() {
-  
+
+  const float pi = 3.14315926536;
+  float r;
+  float h = 15;
+
+  cout << "please enter radius :";
+  cin >> r;
+
+  cout << "Area of Circle :" << pi * r * r << endl;
+  cout << "line of circle :" << 2*pi * r << endl;
+
+  cout << "Area of Circle :" <<pi * r * r *h << endl;
   return 0;
 }
